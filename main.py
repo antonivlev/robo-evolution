ac321c739e58c2abaa72361eba7d80f60a9f27d7
anton <anton@codesigned.co.uk>
2019-02-17 13:50:22 +0000

    Set up evolution, add gitignore
    Not sure about the mutation operator; should it make a copy of the individual?

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import runSim
-from robotIndividual import RobotIndividual
+from robotIndividual import RobotIndividual, crossRobots
+
+import array
+import random
+
+import numpy
 
 from deap import algorithms
 from deap import base
@@ -12,10 +17,32 @@ toolbox = base.Toolbox()
 
 # Structure initializers
 toolbox.register("population", tools.initRepeat, list, creator.Individual)
-pop = toolbox.population(n=300)
 
-# p = runSim.getRobotPosition(nsecs=0.05)
-# print("position: ", p)
-r = RobotIndividual()
-r.sayMyName()
-print("ok")
+toolbox.register("evaluate", RobotIndividual.evaluate)
+# toolbox.register("mate", crossRobots)
+toolbox.register("mutate", RobotIndividual.mutate, prob=0.5)
+toolbox.register("select", tools.selTournament, tournsize=3)
+
+def main():
+    random.seed(64)
+    
+    pop = toolbox.population(n=300)
+    hof = tools.HallOfFame(1)
+    stats = tools.Statistics(lambda ind: ind.fitness.values)
+    stats.register("avg", numpy.mean)
+    stats.register("std", numpy.std)
+    stats.register("min", numpy.min)
+    stats.register("max", numpy.max)
+    
+    pop, log = algorithms.eaSimple(pop, toolbox, cxpb=0.0, mutpb=0.5, ngen=40, 
+                                   stats=stats, halloffame=hof, verbose=True)
+    
+    return pop, log, hof
+
+pop, log, hof = main()
+
+# # p = runSim.getRobotPosition(nsecs=0.05)
+# # print("position: ", p)
+# r = RobotIndividual()
+# RobotIndividual.mutate(r, 0.5)
+# print("ok")
--- robotIndividual.py
+++ robotIndividual.py
@@ -2,8 +2,21 @@ import random
 
 class RobotIndividual:
     def __init__(self):
-        self.name = "dave"
         self.points = [ random.random()*10 for _ in range(10) ]
 
-    def sayMyName(self):
-        print(self.name)
+    def mutate(self, prob):
+        mutatePoint = lambda x: x+1 if random.random() <= 0.5 else x-1
+
+        self.points = [
+            mutatePoint(point) if random.random()<=prob else point 
+            for point in self.points
+        ]
+
+        return self,
+    
+    def evaluate(self):
+        return sum(self.points),
+
+
+def crossRobots(robot1, robot2):
+    return robot1, robot2

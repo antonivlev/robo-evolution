2e9f58def15fbb44077857284fdfe3570f34443d
anton <anton@codesigned.co.uk>
2019-04-03 12:28:44 +0100

    Add gaussians for point generation

--- robotIndividual.py
+++ robotIndividual.py
@@ -3,23 +3,46 @@ import numpy as np
 import csv
 from funcsForEvaluation import getRobotPosition
 
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+
 class RobotIndividual:
     def __init__(self):
         # first 5 are for the organs.
         organs = [ [random.random()*10 for _ in range(3)] for _ in range(5) ]
+
+        self.points = np.array([[5, 5, 5]])
+        for _ in range(300):
+            self.points = np.append(
+                self.points, 
+                [ 
+                    [   
+                        np.random.normal(loc=5, scale=2), 
+                        np.random.normal(loc=5, scale=2), 
+                        np.random.normal(loc=5, scale=2)
+                    ]
+                ],
+                axis=0
+            )
+
+        ax.scatter(self.points[:, 0], self.points[:, 1], self.points[:, 2])
+        plt.show()
         # z is vertical
         # self.points = organs + [ 
         #     [0, 0, 0],
-        #     [0, 10, 0],
-        #     [10, 0, 0],
-        #     [10, 10, 0],
+        #     [2, 0, 0],
+        #     [4, 0, 0],
+        #     [6, 0, 0],
 
-        #     [0, 0, 10],
-        #     [0, 10, 10],
-        #     [10, 0, 10],
-        #     [10, 10, 10],
+        #     # [0, 0, 10],
+        #     # [0, 10, 10],
+        #     # [10, 0, 10],
+        #     # [10, 10, 10],
         # ]
-        self.points = organs + [[x, 0, 0] for x in np.linspace(0, 10, 5)]
+        # self.points = organs + [[x, 0, 0] for x in np.linspace(0, 10, 5)]
 
 
     def mutate(self, prob):
